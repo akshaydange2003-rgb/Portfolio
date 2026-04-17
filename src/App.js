a0a49ac8d7d54d3a039d448192197d888203c4d3
akshaydange2003-rgb <akshaydange2003@gmail.com>
2026-04-17 18:04:19 +0530

    Initial commit - portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import styled, {ThemeProvider}from "styled-components";
+import { darkTheme ,lightTheme} from './utils/Themes';
+import Navbar from "./components/Navbar";
+import Hero from './components/HeroSection';
+import Skills from './components/Skills';
+import Experience from "./components/Experience";
+import Education from './components/Education';
+import Footer from "./components/Footer";
+import { BrowserRouter as Router} from "react-router-dom"
+import ProjectDetails from "./components/ProjectDetails";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 
 function App() {
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <ThemeProvider theme={darkTheme ? darkTheme : lightTheme}>
+      <Router>
+      <Navbar />
+    <Body>
+      <Hero/>
+      <Wrapper>
+      <Skills/>
+      <Experience />
+      </Wrapper>
+      <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Education />
+            <Contact />
+          </Wrapper>
+      <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
+    </Body>
+    </Router>
+   
+    </ThemeProvider>
+  )
 }
 
 export default App;
